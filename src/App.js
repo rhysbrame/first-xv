e46dec636f41c6984467c62e83ccb03907820b9b
Rhys Brame <rhys.brame@ros.gov.uk>
2018-07-11 15:31:35 +0100

    removed unused component import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, NavLink, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Player from "./components/Player";
