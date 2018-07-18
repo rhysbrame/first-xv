5e73f1c160d0be433352d14ae365acf6ec3838a9
Rhys Brame <rhys.brame@ros.gov.uk>
2018-07-18 12:03:35 +0100

    added express server

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
@@ -8,27 +8,50 @@ import Roster from "./components/Roster";
 import "./App.css";
 import logo from "./logo.svg";
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to First XV</h1>
-      <NavBar className="nav-bar" />
-    </header>
-    <main>
-      <h1 className="App-intro">
-        To get started, select the form of the players and let us show you your
-        first XV.
-      </h1>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/player" component={FirstTeam} />
-        <Route path="/player/:number" component={Player} />
-        <Route path="/roster" component={Roster} />
-        <Route path="/firstteam" component={FirstTeam} />
-      </Switch>
-    </main>
-  </div>
-);
+class App extends Component {
+  state = {
+    response: ""
+  };
 
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch("/api/hello");
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to First XV</h1>
+          <NavBar className="nav-bar" />
+        </header>
+        <main>
+          <h1 className="App-intro">{this.state.response}</h1>
+          <h2>
+            To get started, select the form of the players and let us show you
+            your first XV.
+          </h2>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/player" component={FirstTeam} />
+            <Route path="/player/:number" component={Player} />
+            <Route path="/roster" component={Roster} />
+            <Route path="/firstteam" component={FirstTeam} />
+          </Switch>
+        </main>
+      </div>
+    );
+  }
+}
 export default App;
