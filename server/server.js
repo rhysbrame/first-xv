bdcf19f9641c859ddfd740b622aa223306688e7c
Rhys Brame <rhys.brame@ros.gov.uk>
2018-08-04 14:26:03 +0100

    changed the url builder in routes

--- server/server.js
+++ server/server.js
@@ -2,18 +2,19 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
-require("./routes")(app);
+const season = require("./routes/season");
+const teamProfile = require("./routes/teamProfile");
+const player = require("./routes/player");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/team", require("./routes/team"));
+
+app.use(season);
+app.use(teamProfile);
+app.use(player);
 
 app.get("/", (req, res) => {
   res.send("Express server Home page");
-});
-
-app.get("/about", (req, res) => {
-  res.send("about page for the Express server");
 });
 
 app.listen(port, err => {
