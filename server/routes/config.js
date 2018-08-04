9a177042879735e32833853fe327a79edcd62f54
Rhys Brame <rhys.brame@ros.gov.uk>
2018-08-04 14:25:15 +0100

    added new routes

---
+++ server/routes/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  query: {
+    teamProfileQuery: "/teams/sr:competitor:4205/profile.json?",
+    seasonQuery: "/seasons.json?",
+    seasonStatsQuery: "/seasons/sr:season:44378/live_standings.json?"
+  },
+  API: {
+    key: "czvunrws6gmkaa5veyk4qchk"
+  }
+};
---
+++ server/routes/player.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+
+const config = require("./config");
+const fetch = require("node-fetch");
+
+router.get("/player", (req, res) => {
+  const query = config.query.seasonStatsQuery;
+  const apiKey = config.API.key;
+
+  const url = `https://api.sportradar.us/rugby/trial/v2/union/en${query}api_key=${apiKey}`;
+
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      res.send({
+        data
+      });
+    })
+    .catch(err => {
+      res.redirect("/error");
+    });
+});
+
+module.exports = router;
